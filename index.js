f8a7b48f5df51ffcf03a914caf29bde729b5a71a
Christian <christantido@gmail.com>
2021-06-30 19:37:48 +0800

    Added UI Layouts and Design

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000
 app.set('view engine', 'ejs')
 
 app.use('/', require('./routes/pages'))
+app.use('/users', require('./routes/users'))
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
