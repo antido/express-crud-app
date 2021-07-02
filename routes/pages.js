57021edc3986a2cc5728bace2aa0e3d56a2a992b
Christian <christantido@gmail.com>
2021-07-02 13:48:52 +0800

    Finish CRUD functionality for users

--- routes/pages.js
+++ routes/pages.js
@@ -1,12 +1,26 @@
 const express = require('express')
+const mysql = require('mysql')
+const connection = require('../database/connection')
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('index')
 })
 
+// Fetch Data and Redirect to Dashboard
 router.get('/dashboard', (req, res) => {
-    res.render('main')
+    connection.query('SELECT * FROM users', (err, rows, fields) => {
+        if (err) {
+            console.log(`Failed to execute query: ${err}`)
+            res.sendStatus(500)
+            res.end()
+
+            return
+        }
+
+        res.render('main', { data: rows })
+    })
 })
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,27 @@ const router = express.Router()
 
 router.use(express.urlencoded({ extended: false }))
 
-// Display Create User Page
-router.get('/add', (req, res) => {
-    res.render('addUser')
+// Show Specific User Data
+router.get('/show/:id', (req, res) => {
+    connection.query('SELECT * FROM users WHERE id=?', [req.params.id], (err, rows, fields) => {
+        if (err) {
+            console.log(`Failed to execute query: ${err}`)
+            res.sendStatus(500)
+            res.end()
+
+            return
+        }
+
+        res.render('users/show', { data: rows })
+    })
 })
 
-// Insert Data
+// Create User Page
+router.get('/create', (req, res) => {
+    res.render('users/create')
+})
+
+// Insert Data 
 router.post('/store', (req, res) => {
     const firstname = req.body.firstName
     const lastname = req.body.lastName
@@ -32,6 +47,63 @@ router.post('/store', (req, res) => {
         }
 
         res.redirect('/dashboard')
+    })
+})
+
+// Edit Specific User Page
+router.get('/edit/:id', (req, res) => {
+    connection.query('SELECT * FROM users WHERE id=?', [req.params.id], (err, rows, fields) => {
+        if (err) {
+            console.log(`Failed to execute query: ${err}`)
+            res.sendStatus(500)
+            res.end()
+
+            return
+        }
+
+        res.render('users/edit', { data: rows })
+    })
+})
+
+// Update Specific User
+router.post('/update', (req, res) => {
+    const id = req.body.userId
+    const firstname = req.body.firstName
+    const lastname = req.body.lastName
+    const email = req.body.email
+    const birthdate = req.body.birthDate
+    const address = req.body.address
+    const username = req.body.username
+    const password = req.body.password 
+
+    const query = 'UPDATE users SET first_name = ?, last_name = ?, email = ?, birth_date = ?, address = ?, username = ?, password = ? WHERE id = ?'
+    connection.query(query, [firstname, lastname, email, birthdate, address, username, password, id], (err, result, fields) => {
+        if (err) {
+            console.log(`Failed to execute query: ${err}`)
+            res.sendStatus(500)
+            res.end()
+
+            return
+        }
+
+        res.redirect('/dashboard')
+    })
+})
+
+
+// Delete Specific User
+router.delete('/destroy/:id', (req, res) => {
+    connection.query('DELETE FROM users WHERE id=?', [req.params.id], (err, result, fields) => {
+        if (err) {
+            console.log(`Failed to execute query: ${err}`)
+            res.sendStatus(500)
+            res.end()
+
+            return
+        }
+
+        res.send({ message: 'User Deleted!' })
+        // res.redirect('/dashboard')
     })
 })
 
